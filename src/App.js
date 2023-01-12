3d05655386e0ff6bf897b9794eebe027e9ec0543
idrissheikh-idris <s310058@oslomet.no>
2023-01-12 16:04:22 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import React from 'react';
+import Cookies from './components/Cookies';
+
+function App() {
+  return (
+    <div className="">
+      <Cookies/>
+    
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Cookies.js
@@ -0,0 +1,19 @@
+import React from 'react'
+import './Cookies.css'
+
+function Cookies() {
+  return (
+    <div className='cookies-card'>
+        <p className="title">Cookies content</p>
+        <p className="info">We use cookies to personalise content and ads, to provide social media features and to analyse our traffic.
+            <a href='#'className='a' >Read more</a> </p>
+            <button className="cbtn">
+                Accept All
+            </button>
+            <a href='#' className='setting'>Cookies settings</a>
+
+    </div>
+  )
+}
+
+export default Cookies
